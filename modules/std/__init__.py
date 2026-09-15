97d1a693693728d79612decd27d8dc20f9f70957
Dora Kökçe <dorakokce@gmail.com>
2026-09-15 17:27:44 +0300

    Venv and Python side changes
    Added venv struct to help manage venv's. Made node decorator accept id and register it with the module so the names don't crash

--- modules/std/__init__.py
+++ modules/std/__init__.py
@@ -13,18 +13,38 @@ class Socket:
         return self.ty is Exec
 
 def _to_sock(v: Socket | type):
-    if v is Socket:
+    if isinstance(v, Socket):
         return v
-    if v is type:
+    if isinstance(v, type):
         return Socket(v)
 
-def node(inputs: dict[str, Socket | type], outputs: dict[str, Socket | type]):
+NODE_REGISTRY = {}
+
+def populate_registry(id: str, func):
+    keys = id.split('.')
+    current = NODE_REGISTRY
+
+    for key in keys[:-1]:
+        if key not in current or not isinstance(current[key], dict):
+            if key in current and not isinstance(current[key], dict):
+                raise ValueError(f"path conflict: '{key}' is already registered as a node function.")
+            current[key] = {}
+        current = current[key]
+
+    if keys[-1] in current and isinstance(current[keys[-1]], dict):
+        raise ValueError(f"path conflict: '{keys[-1]}' is already a module namespace, cannot attach node.")
+
+    current[keys[-1]] = func
+
+def node(id: str, inputs: dict[str, Socket | type], outputs: dict[str, Socket | type]):
     def wrapper(func):
         if not inspect.isfunction(func):
             raise TypeError(f"@node only allows functions. got: {type(func)}")
 
         func._node_inputs = {k: _to_sock(v) for k, v in inputs.items()} # type: ignore
         func._node_outputs = {k: _to_sock(v) for k, v in outputs.items()} # type: ignore
+
+        populate_registry(func.__module__ + '.' + id, func)
 
         return func
     return wrapper
--- modules/std/math.py
+++ modules/std/math.py
@@ -1,9 +1,10 @@
-#from std import Socket, node
+from std import Socket, node
 
 
-#@node({"x": Socket(object, True), "y": Socket(object, True)}, {"x+y": object})
+@node("math.add", {"x": Socket(object, True), "y": Socket(object, True)}, {"x+y": object})
 def add(ctx):
     return {"x+y": ctx["x"] + ctx["y"]}
 
+@node("math.sub", {"x": Socket(object, True), "y": Socket(object, True)}, {"x-y": object})
 def sub(ctx):
     return {"x-y": ctx["x"] - ctx["y"]}
